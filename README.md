c2585d012bbab2229de613f2f0a47db80a0e6c63
Yattabyte <tklowry93@gmail.com>
2020-03-02 15:55:08 -0600

    0.0.1
    First commit, initializing with some basic code and external dependencies (glfw + glad)

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Linux](https://img.shields.io/travis/yattabyte/MiniGFX?label=Linux%20Build&logo=Travis)](https://travis-ci.com/Yattabyte/MiniGFX)
+[![Windows](https://img.shields.io/appveyor/ci/yattabyte/MiniGFX?label=Windows%20Build&logo=Appveyor)](https://ci.appveyor.com/project/Yattabyte/MiniGFX)
+[![CodeFactor](https://img.shields.io/codefactor/grade/github/yattabyte/MiniGFX?label=Code%20Factor&logo=CodeFactor)](https://www.codefactor.io/repository/github/yattabyte/MiniGFX)
+[![Codacy](https://img.shields.io/codacy/grade/423d5e4bdb064914aef2589a3e58f483?label=Code%20Quality&logo=Codacy)](https://www.codacy.com/manual/Yattabyte/MiniGFX)
+[![LGTM](https://img.shields.io/lgtm/grade/cpp/github/Yattabyte/MiniGFX?label=Code%20Quality&logo=LGTM)](https://lgtm.com/projects/g/Yattabyte/MiniGFX)
+[![CodeDocs](https://codedocs.xyz/Yattabyte/MiniGFX.svg)](https://codedocs.xyz/Yattabyte/MiniGFX/)
+[![license](https://img.shields.io/github/license/Yattabyte/MiniGFX?label=License&logo=github)](https://github.com/Yattabyte/MiniGFX/blob/master/LICENSE)
---
+++ src/shader.cpp
@@ -0,0 +1,73 @@
+#include "shader.hpp"
+#include <glad/glad.h>
+#include <vector>
+
+Shader::~Shader() {
+    glDeleteShader(m_vertexID);
+    glDeleteShader(m_fragmentID);
+    glDeleteProgram(m_programID);
+}
+
+Shader::Shader(
+    const std::string& vertexSource, const std::string& fragmentSource) noexcept
+    : m_vertexID(glCreateShader(GL_VERTEX_SHADER)),
+      m_fragmentID(glCreateShader(GL_FRAGMENT_SHADER)),
+      m_programID(glCreateProgram()) {
+    // Make vertex shader
+    const auto v_cstr = vertexSource.c_str();
+    glShaderSource(m_vertexID, 1, &v_cstr, nullptr);
+    glCompileShader(m_vertexID);
+
+    // Make fragment shader
+    const auto f_cstr = fragmentSource.c_str();
+    glShaderSource(m_fragmentID, 1, &f_cstr, nullptr);
+    glCompileShader(m_fragmentID);
+
+    // Create program
+    glAttachShader(m_programID, m_vertexID);
+    glAttachShader(m_programID, m_fragmentID);
+
+    // Link program
+    glLinkProgram(m_programID);
+
+    // Validate program
+    GLint param(0);
+    if (glGetProgramiv(m_programID, GL_INFO_LOG_LENGTH, &param); param != 0) {
+        std::vector<GLchar> infoLog(param);
+        glGetProgramInfoLog(
+            m_programID, static_cast<GLsizei>(param), nullptr, &infoLog[0]);
+        m_log = infoLog.data();
+    }
+    glValidateProgram(m_programID);
+    glDetachShader(m_programID, m_vertexID);
+    glDetachShader(m_programID, m_fragmentID);
+}
+
+bool Shader::valid() const noexcept {
+    if (m_vertexID == 0 || m_fragmentID == 0 || m_programID == 0)
+        return false;
+
+    GLint param(0);
+    glGetProgramiv(m_programID, GL_LINK_STATUS, &param);
+    return param != 0;
+}
+
+std::string Shader::errorLog() const { return m_log; }
+
+void Shader::bind() const noexcept { glUseProgram(m_programID); }
+
+void Shader::uniformLocation(const int& location, const vec3& vector) const
+    noexcept {
+    glProgramUniform3fv(m_programID, location, 1U, vector.data());
+}
+
+void Shader::uniformLocation(const int& location, const vec4& vector) const
+    noexcept {
+    glProgramUniform4fv(m_programID, location, 1U, vector.data());
+}
+
+void Shader::uniformLocation(const int& location, const mat4& matrix) const
+    noexcept {
+    glProgramUniformMatrix4fv(
+        m_programID, location, 1, GL_FALSE, matrix.data());
+}
